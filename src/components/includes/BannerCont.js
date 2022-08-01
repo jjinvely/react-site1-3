d5cbe6ac24c978861663f573e63da4dea0b67e3d
yeojin Kim <jjiny2022@kakao.com>
2022-08-01 12:47:34 +0900

    220801-3

--- src/components/includes/BannerCont.js
+++ src/components/includes/BannerCont.js
@@ -1,6 +1,17 @@
 import React from 'react'
 
 
+
+
+const BannerTitle= [
+  {text: "유튜버 웹보이"},
+  {desc: "더 다양한 강의는 유튜블 통해 제공하고 있습니다."},
+  {address:"youtube.com/c/Webstoryboy"},
+  {sub:"배너 영역01"}
+]
+
+
+
 function BannerCont(props) {
   return (
     <div>
@@ -10,12 +21,12 @@ function BannerCont(props) {
       >
         <h2 className="ir_so">배너 영역</h2>
         <div className="banner__inner">
-          <h3 className="banner__title">유튜버 웹보이</h3>
+          <h3 className="banner__title">{BannerTitle[0].text}</h3>
           <p className="banner__desc">
-            더 다양한 강의는 유튜브를 통해 제공하고 있습니다.
-            <a href="/">youtube.com/c/Webstoryboy</a>
+            {BannerTitle.desc}
+            <a href="/">{BannerTitle[0].address}</a>
           </p>
-          <span className="banner__sub">배너 영역01</span>
+          <span className="banner__sub">{BannerTitle[0].sub}</span>
         </div>
       </section>
     </div>
